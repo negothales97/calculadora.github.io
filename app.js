15439f7b2558478c67f3d9cb497a92e93608f414
Nego Thales <thales@imaxinformatica.com.br>
2021-08-06 14:43:38 -0300

    primeira refatoracao

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const inputTotal = document.querySelector('#total');
+
 const calculator = (e) => {
-    const inputTotal = document.querySelector('#total');
     let clicked = e.target.innerHTML;
 
     let reset = localStorage.getItem('reset');
@@ -9,41 +10,28 @@ const calculator = (e) => {
             inputTotal.value = '0';
         }
     }
-
-    if (clicked === 'DEL') {
-        inputTotal.value = inputTotal.value.substring(0, inputTotal.value.length - 1);
-    }
-    if (clicked === 'C') {
-        inputTotal.value = '0';
-    }
-
     if (!isNaN(clicked)) {
         if (inputTotal.value == 0) {
             inputTotal.value = clicked;
         } else {
             inputTotal.value += clicked;
         }
-    } else {
-        if (clicked === '+' || clicked === '-' || clicked === '/' || clicked === '*') {
-            if (inputTotal.value.slice(-1) === '+' || inputTotal.value.slice(-1) === '-' || inputTotal.value.slice(-1) === '/' || inputTotal.value.slice(-1) === '*') {
-                inputTotal.value = inputTotal.value.substring(0, inputTotal.value.length - 1);
-            }
-            if (inputTotal.value == 0) {
-                inputTotal.value = 0;
-            } else {
-                inputTotal.value += clicked;
-            }
-        }
-        if (clicked === '=') {
-            let result = eval(inputTotal.value);
-            inputTotal.value = result;
-            localStorage.setItem('reset', 'true');
-        }
     }
 }
-const executeAction = () => {
+const executeAction = (e) => {
+    const typeActions = ['+', '-', '/', '*'];
 
+    if (typeActions.includes(inputTotal.value.slice(-1))) {
+        inputTotal.value = inputTotal.value.substring(0, inputTotal.value.length - 1);
+    }
+    inputTotal.value = inputTotal.value == 0 ? 0 : inputTotal.value + e.target.dataset.action;
 }
+const showResult = (e) => inputTotal.value = eval(inputTotal.value); //Mostra o resultado
+const clear = () => inputTotal.value = '0'; //Limpa o resultado na tela
+const backspace = () => inputTotal.value = inputTotal.value.substring(0, inputTotal.value.length - 1); // limpa o último digito
 
 document.querySelectorAll('button').forEach(button => button.addEventListener("click", calculator));
-// document.querySelectorAll('.action').forEach(button => button.addEventListener("click", executeAction));
+document.querySelectorAll('.action').forEach(action => action.addEventListener("click", executeAction));
+document.querySelector('.result').addEventListener('click', showResult);
+document.querySelector('.backspace').addEventListener('click', backspace);
+document.querySelector('.clear').addEventListener('click', clear);
